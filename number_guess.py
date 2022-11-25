13178380ebaf221583814b3bb1c5e5ba4220244d
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-11-24 18:08:05 -0800

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,20 +1,22 @@
-
-
 import random
-
+# global health variables
 health_easy = 10
 health_hard = 5
 
+# function to compere guess and number
 def compare(number, guess, turns):
     if guess > number:
         print('Too high.')
+        # decrease turns by 1
         return turns -1
     elif guess < number:
         print('Too low.')
+        # decrease turns by 1
         return turns -1
     else:
         print(f'You win! the answer was {number}')
 
+# function to set difficulty -> returns health_easy or health_hard
 def set_difficulty():
     level = input("Choose a difficulty. Type 'easy' or 'hard': ")
     if level == "easy":
@@ -22,20 +24,15 @@ def set_difficulty():
     else:
         return health_hard
 
+# main game function
 def game():
     number = random.randint(1, 100)
     print(f"Pssst, the correct answer is {number}") 
-
     turns = set_difficulty()
-    #Repeat the guessing functionality if they get it wrong.
     guess = 0
     while guess != number:
         print(f"You have {turns} attempts remaining to guess the number.")
-
-        #Let the user guess a number.
         guess = int(input("Make a guess: "))
-
-        #Track the number of turns and reduce by 1 if they get it wrong.
         turns = compare(number, guess, turns)
         if turns == 0:
             print("You've run out of guesses, you lose.")
