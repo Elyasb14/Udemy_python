243c061e57899218d71187d3bb11cc2baeaa6d92
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-11-26 09:56:26 -0800

    Verson 1.0

--- higher_or_lower/higher_or_lower.py
+++ higher_or_lower/higher_or_lower.py
@@ -1,10 +1,30 @@
 from game_data import data
 import random
+
 score = 0
-person1 = random.choice(data)
-# print(person1)
-followers_1 = person1['follower_count']
-# print(followers_1)
 end_of_game = False
 
+def compare(followersA, followersB):
+    if followersA > followersB:
+        return 'A'
+    else: 
+        return 'B'
+    
+
 while not end_of_game:
+    personA = random.choice(data)
+    personA_name = personA['name']
+    followersA = personA['follower_count']
+    personB = random.choice(data)
+    personB_name = personB['name']
+    followersB = personB['follower_count']
+    print(f'{personA_name} vs. {personB_name}')
+    print(f'person {compare(followersA, followersB)} is the answer.')
+    guess = input('Who has more followers, person A or B: ')
+    answer = compare(followersA, followersB)
+    if guess == answer:
+        score += 1
+        print(f'Your score is {score}')
+    else:
+        print('You were wrong')
+        end_of_game = True
