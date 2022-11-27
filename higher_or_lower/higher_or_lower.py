836dbd30791c96db41b0138af46adf0d292c6a7b
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-11-26 16:07:58 -0800

    Update higher_or_lower.py

--- higher_or_lower/higher_or_lower.py
+++ higher_or_lower/higher_or_lower.py
@@ -13,18 +13,19 @@ end_of_game = False
 
 def compare(followersA, followersB):
     if followersA > followersB:
-        return 'A'
+        return 'A', personA
     else: 
-        return 'B'
-    
+        return 'B', personB
+
+personA = random.choice(data)
+personA_name = personA['name']
+followersA = personA['follower_count']
+
+personB = random.choice(data)
+personB_name = personB['name']
+followersB = personB['follower_count']
 
 while not end_of_game:
-    personA = random.choice(data)
-    personA_name = personA['name']
-    followersA = personA['follower_count']
-    personB = random.choice(data)
-    personB_name = personB['name']
-    followersB = personB['follower_count']
     print(f'{personA_name} vs. {personB_name}')
     print(f'person {compare(followersA, followersB)} is the answer.')
     guess = input('Who has more followers, person A or B: ')
@@ -32,6 +33,8 @@ while not end_of_game:
     if guess == answer:
         score += 1
         print(f'Your score is {score}')
+        personA = answer
+        personB = random.choice(data)
     else:
         print('You were wrong')
         end_of_game = True
