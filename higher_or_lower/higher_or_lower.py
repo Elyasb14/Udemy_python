cc7d3602e4be5b4b6c9826224ff6ead89a301786
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-11-26 16:30:19 -0800

    This version works

--- higher_or_lower/higher_or_lower.py
+++ higher_or_lower/higher_or_lower.py
@@ -34,19 +34,19 @@ def game():
         accountB = get_account()
         while accountA == accountB:
             accountB = get_account()
-            print(f"Compare A: {format_data(accountA)}.")
-            print('vs')
-            print(f"Against B: {format_data(accountB)}.")
-            guess = input('Input who you think has more followers: ').lower()
-            followersA = accountA['follower_count']
-            followersB = accountB['follower_count']
-            is_correct = compare(followersA, followersB, guess)
-            if is_correct:
-                score += 1
-                print(f"You're right! Current score: {score}.")
-            else:
-                end_of_game = True
-                print('you lose...')
+        print(f"Compare A: {format_data(accountA)}.")
+        print('vs')
+        print(f"Against B: {format_data(accountB)}.")
+        guess = input('Input who you think has more followers: ').lower()
+        followersA = accountA['follower_count']
+        followersB = accountB['follower_count']
+        is_correct = compare(followersA, followersB, guess)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            end_of_game = True
+            print('you lose...')
                 
 game()
                 
