7af09c936295fab26bfa4de758887300fd9d4345
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-11-28 20:26:32 -0800

    commit

--- coffee_machine/coffee_machine.py
+++ coffee_machine/coffee_machine.py
@@ -1,28 +1,51 @@
 from data import MENU, resources
 
-def espresso():
-    return water - 100, coffee - 50
+def request_money(choice):
+    if choice == 'espresso':
+        cost = MENU['espresso']['cost']
+        money_request = float(input(f'input ${cost}: '))
+        money = money_request
+        return money
+    elif choice == 'latte':
+        cost = MENU['latte']['cost']
+        money_request = float(input(f'input ${cost}: '))
+        money = money_request
+        return money
+    else:
+        cost = MENU['cappuccino']['cost']
+        money_request = float(input(f'input ${cost}: '))
+        money = money_request
+        return money
 
-def latte():
-    return water - 100, coffee - 50, milk - 50
-
-def cappuccino():
-    return water - 100, coffee - 50, milk -100
-
-def input_money():
-    pass
+def check_ingredients(choice, water, milk, coffee):
+    if MENU['resources']['water'] >  MENU[choice]['ingredients']['water']:
+        MENU['resources']['water'] = MENU['resources']['water'] - MENU[choice]['ingredients']['water']
+        
+        
+    
 
 def coffee_machine():
     machine_off = False
-    water = resources['water']
-    milk = resources['milk']
-    coffee = resources['coffee']
     money = 0
-    while not machine_off:
-        choice = input('What would you like? (espresso/latte/cappuccino): ').lower()
-        if choice == 'off':
-            return
-        elif choice == 'report':
-            print(f'Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}')
+    choice = input('What would you like to drink: ').lower()
+    water = MENU[choice]['ingredients']['water']
+    milk = MENU[choice]['ingredients']['milk']
+    coffee = MENU[choice]['ingredients']['coffee']
 
+    if choice == 'off':
+        return
+    elif choice == 'status':
+        print(f'Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}')    
+    elif choice == 'espresso':
+        money = request_money(choice)
+        
+        # print(money)
+    elif choice == 'latte':
+        money = request_money(choice)
+        # print(money)
+    else: 
+        money = request_money(choice)
+        # print(money)
+    
+        
 coffee_machine()
