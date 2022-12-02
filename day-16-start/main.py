89e01ba333c1fd143a83056613bd6e2a3b6543cc
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-12-01 17:49:46 -0800

    yes

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,4 +1,7 @@
-from turtle import Turtle, Screen
+
+'''from turtle import Turtle, Screen
+
+
 timmy = Turtle()
 print(timmy)
 timmy.shape('turtle')
@@ -7,4 +10,17 @@ my_screen = Screen()
 
 print(my_screen.canvheight)
 
-my_screen.exitonclick()
+timmy.color('Green', 'Red')
+
+timmy.forward(100)
+
+my_screen.exitonclick()'''
+
+import prettytable
+
+table = prettytable.PrettyTable()
+
+table.add_column('Pokemon name', ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('Type', ['electric', 'water', 'fire'])
+table.align = 'l'
+print(table)
