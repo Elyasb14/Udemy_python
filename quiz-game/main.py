e445c71722fd6584b6808acdef176d706c06050f
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-12-03 18:32:00 -0800

    uodate

--- quiz-game/main.py
+++ quiz-game/main.py
@@ -1,9 +1,17 @@
-from question_model import Question
 from data import question_data
+from question_model import Question
+import numpy
 
 question_bank = []
 for data in question_data:
     question = Question(data['text'], data['answer'])
     question_bank.append(question)
     
-print(question_bank[1].answer)
+# print(question_bank[1].answer)
+
+
+
+
+x = numpy.array([[[1,1,2], [2,3,3]], [[1,1,1], [1,1,1]]])
+print(x.shape)
+print(x)
---
+++ quiz-game/quiz_brain.py
@@ -0,0 +1,13 @@
+class QuizBrain:
+    
+    def __init__(self, question_bank):
+        self.question_number = 0
+        self.questions_list = question_bank
+    
+    def next_question(self):
+        question = data[self.question_number]
+        
+        
+
+
+print(data[0])
