9e7c0a42feed78dc295461fcdc7fae859998205b
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-12-04 18:28:48 -0800

    commit

--- quiz-game/quiz_brain.py
+++ quiz-game/quiz_brain.py
@@ -1,13 +1,25 @@
+from data import question_data
 class QuizBrain:
     
     def __init__(self, question_bank):
         self.question_number = 0
-        self.questions_list = question_bank
-    
+        self.question_list = question_bank
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
-        question = data[self.question_number]
-        
-        
+        question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f'Q.{self.question_number}: {question.text} (True/False): ')
+        self.check_answer(user_answer, question.answer)
 
-
-print(data[0])
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print('Right')
+        else:
+            print('wrong')
+        print(f'The correct answer is: {correct_answer}')
+        
