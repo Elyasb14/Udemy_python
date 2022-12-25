99fa634b2a2f2e27e5c36c27a7cb6352cf648309
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-12-24 23:28:47 -0800

    version 1.0

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -2,27 +2,37 @@ import turtle
 import pandas as pd
 import time
 
+
+'''
+TO DO:
+- make it so the turtle doesn't flash every time its created
+'''
+
+
 score = 0
 
+# initialize screen
 screen = turtle.Screen()
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-print(screen.screensize())
-
 # turn into dataframe
 data = pd.read_csv("50_states.csv")
 
+# conditional for game
 game_is_on = True
 while game_is_on:
     screen.title(f"US States Game: {score}/50")
+    # check if you've won the game
     if score == 50:
+            # print you win on the screen
             drawing = turtle.Turtle()
             drawing.ht()
             drawing.penup()
             drawing.goto((0, 300))
             drawing.write(arg = "You Win!", align = "Center", font = ("Arial", 24, "normal"))
+            # Do a countdown til screen exits
             for num in [5, 4, 3, 2, 1]:
                 count = turtle.Turtle()
                 count.ht()
@@ -31,9 +41,13 @@ while game_is_on:
                 count.write(arg = f"{num}", align = "Center", font = ("Arial", 24, "normal"))
                 time.sleep(1)
                 count.clear()
+            # end game 
             game_is_on = False
+    # if haven't won game, keep asking for inputs
     else:
+        # prompt input
         answer_state = screen.textinput(title = "Guess the State" , prompt = "Enter State:")
+        # check if input is a state
         if (data["state"] == f"{answer_state}").any():
             score += 1
             state = data[data["state"] == f"{answer_state}"]
@@ -44,6 +58,7 @@ while game_is_on:
             drawing.penup()
             drawing.goto((x, y))
             drawing.write(arg = f"{answer_state}", move = True)
+        # if not, continue
         else:
             continue
 
