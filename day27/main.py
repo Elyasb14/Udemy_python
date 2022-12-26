d077129d28a0dcfcb9f88bf8a63c74b957624579
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2022-12-25 22:10:47 -0800

    commit

---
+++ day27/main.py
@@ -0,0 +1,21 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first gui")
+window.minsize(width = 500, height = 300)
+
+# label
+my_label = tkinter.Label(text = "I am a label", font = ("Garamond", 24))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
+
