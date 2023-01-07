690b40bc19848d21c3369af5d707af41384af863
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-01-06 17:08:11 -0800

    commit

--- NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
+++ NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
@@ -6,7 +6,7 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+# print(phonetic_dict)
 while True:
     try:
         word = input("Enter a word: ").upper()
